1e931c4ad54af3ce672138bc530ade9422e32aad
Mathieu Urstein <mathieu.urstein@gmail.com>
2017-01-22 16:58:54 +0100

    New code structure for api testing

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,28 @@
 import { Component } from '@angular/core';
+import { ApiCallsService } from './api/ApiCalls.service';
+import {GamifiedApplication} from "./model/GamifiedApplication";
 
 @Component({
   selector: 'my-app',
   template: `<h1>Hello {{name}}</h1>`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+  name = 'Angular';
+
+  constructor(
+    private apiCallsService: ApiCallsService
+  ) {
+    let gameApp: GamifiedApplication = {
+      name: "testtest7",
+      password: "testtest4"
+    };
+    this.apiCallsService.registerPost(gameApp).subscribe(
+      res => {
+        console.log(res);
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { ApiCallsService } from './api/ApiCalls.service';
+import {HttpModule} from "@angular/http";
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, HttpModule ],
   declarations: [ AppComponent ],
+  providers:    [ ApiCallsService ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
