5e7062e8ef557296f57dbbd44db02b348507887f
Mathieu Urstein <mathieu.urstein@gmail.com>
2017-01-23 08:49:43 +0100

    Users endpoint

--- app/endpoints/users.component.ts
+++ app/endpoints/users.component.ts
@@ -5,18 +5,40 @@ import { ApiCallsService } from '../api/ApiCalls.service';
   selector: 'users',
   template: `
 <form>
-  Get users of the current application
+  <p>Get users of the current application</p>
   <button (click)="getUsers()" type="submit" class="btn btn-primary">Submit</button>
+</form>
+<hr>
+<form>
+  <div class="form-group">
+    <label>Username</label>
+    <input name="username" [(ngModel)]="username" type="text" class="form-control" placeholder="User to retrieve">
+  </div>
+  <button (click)="getUser()" type="submit" class="btn btn-primary">Submit</button>
 </form>
 `,
 })
 export class UsersComponent  {
+
+  username: string;
+
   constructor(
     private apiCallsService: ApiCallsService
   ) {}
 
   getUsers() {
     this.apiCallsService.usersGet().subscribe(
+      res => {
+        console.log(res);
+      },
+      err => {
+        this.apiCallsService.logResponse(err);
+      }
+    );
+  }
+
+  getUser() {
+    this.apiCallsService.usersUsernameGet(this.username).subscribe(
       res => {
         console.log(res);
       },
